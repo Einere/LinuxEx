7b2847efec8f5f2a529d93b9eb41a0107af40b43
Einere <kjwsx23@gmail.com>
2017-11-22 17:22:41 +0900

    [sisosil] Add 3rd example

--- ex10.c
+++ ex10.c
@@ -18,4 +18,36 @@ void* do_loop(void *data){
 	}
 }
 
+void* do_loop2(void *data){
+	int i;
+	for(i = 0; i < 10; i++){
+		pthread_mutex_lock(&mutex);
+		printf("loop2 : %d\n", ncount);
+		ncount++;
+		pthread_mutex_unlock(&mutex);
+		sleep(2);
+	}
+}
+
+int main(){
+	int thr_id;
+	pthread_t p_thread[2];
+	int status;
+	int a = 1;
+
+	ncount = 0;
+	thr_id = pthread_create(&p_thread[0], NULL, do_loop, (void*)&a);
+	sleep(1);
+	thr_id = pthread_create(&p_thread[1], NULL, do_loop2, (void*)&a);
+
+	pthread_join(p_thread[0], (void*)&status);
+	pthread_join(p_thread[1], (void*)&status);
+
+	status = pthread_mutex_destroy(&mutex);
+
+	printf("code = %d\n", status);
+	printf("programing is end\n");
+
+	return 0;
+}
 
