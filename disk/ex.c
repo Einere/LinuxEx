1c98c0f3b45408c7f76cfc51d15ffdae0af752fc
Einere <kjwsx23@gmail.com>
2018-03-23 23:51:07 +0900

    Modify fs.c (set bitmap)

--- disk/ex.c
+++ disk/ex.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define MAX_SIZE 4
+
+void setbit(char* ptr, int bit_index);
 
 int main(){
-	char* ptr = (char*)malloc(4);
-//	memset(ptr, 0, 4);
-	for(int i=3; i>=0; i--){
-		printf("%d...\n", *ptr >> i & 1 );
+	char* ptr = (char*)malloc(MAX_SIZE);
+	memset(ptr, 0, MAX_SIZE);
+	setbit(ptr, 10);
+	
+	for(int i = MAX_SIZE - 1; i >=0; i--){
+		for(int j = 0; j < 8; j++){
+			printf("%d", (ptr[i] >> j) & 1);
+		}
+		printf(" ");
 	}
+	
+	printf("\n");	
 	return 0;
+}
+
+void setbit(char* ptr, int bit_index){
+	int byte_index = bit_index / 8;
+	int shift_num = bit_index % 8;
+	printf("byte_index = %d, shift_num = %d\n", byte_index, shift_num);	
+	*(ptr + byte_index) |= 1 << (8 - 1 - shift_num);
+	//*(ptr + byte_index) |= 1 << shift_num;
 }
--- disk/fs.c
+++ disk/fs.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
 #include "fs.h"
+#include "disk.h"
+#define BLOCK_SIZE 512
+
+void setbit(char* ptr, int bit_index){
+	int byte_index = bit_index / 8;
+	int shift_num = bit_index % 8;
+	printf("byte_index = %d, shift_num = %d\n", byte_index, shift_num);	
+	*(ptr + byte_index) |= 1 << (8 - 1 - shift_num);
+	//*(ptr + byte_index) |= 1 << shift_num;
+	
+	for(int i = MAX_SIZE*8 - 1; i>=0; i--){
+		//printf("%d", (1024 >> i) & 1);
+		printf("%d", (*ptr >> i) & 1);
+		if(i%8 == 0) printf(" ");
+	}
+}
 
 void FileSysInit(void)
 {
-	char* pInit = (char*)malloc(512);
-	memset(pInit, 0, 512);
+	char* pInit = (char*)malloc(BLOCK_SIZE);
+	memset(pInit, 0, BLOCK_SIZE);
 	for(int i = 0; i < 7; i++){ 
 		DevWriteBlock(i, pInit);
 	}
@@ -13,9 +29,9 @@ void FileSysInit(void)
 
 void SetInodeBitmap(int blkno)
 {
-	char* pIB = (char*)malloc(512);
+	char* pIB = (char*)malloc(BLOCK_SIZE);
 	DevReadBlock(INODE_BITMAP_BLK_NUM, pIB);
-	pIB |= (1 >> bit_num + 1); //block number? bit number?
+	setbit(pIB, blkno);
 	DevWriteBlock(INODE_BITMAP_BLK_NUM, pIB);
 	free(pIB);
 }
@@ -25,7 +41,7 @@ void ResetInodeBitmap(int blkno)
 {
 	char* pIB = (char*)malloc(512);
 	DevReadBlock(INODE_BITMAP_BLK_NUM, pIB);
-	pIB &= ~(1 >> bit_num + 1); //block number? bit number?
+	pIB &= ~(1 << (BLOCK_SIZE - 1 - bit_num)); //block number? bit number?
 	DevWriteBlock(INODE_BITMAP_BLK_NUM, pIB);
 	free(pIB);
 
@@ -35,7 +51,7 @@ void SetBlockBitmap(int blkno)
 {
 	char* pBB = (char*)malloc(512);
 	DevReadBlock(BLOCK_BITMAP_BLK_NUM, pBB);
-	pBB |= (1 >> bit_num + 1); //block number? bit number?
+	pBB |= (1 << (BLOCK_SIZE - 1 - bit_num)); //block number? bit number?
 	DevWriteBlock(BLOCK_BITMAP_BLK_NUM, pBB);
 	free(pBB);
 }
@@ -45,7 +61,7 @@ void ResetBlockBitmap(int blkno)
 {
 	char* pBB = (char*)malloc(512);
 	DevReadBlock(BLOCK_BITMAP_BLK_NUM, pBB);
-	pBB &= ~(1 >> bit_num + 1); //block number? bit number?
+	pBB &= ~(1 << (BLOCK_SIZE - 1 - bit_num)); //block number? bit number?
 	DevWriteBlock(BLOCK_BITMAP_BLK_NUM, pBB);
 	free(pBB);
 }
@@ -53,22 +69,33 @@ void ResetBlockBitmap(int blkno)
 
 void PutInode(int blkno, Inode* pInode)
 {
-		
+	char* pBlock = (char*)malloc(BLOCK_SIZE);
+	int block_num = (Inode_num / 8) + INODELIST_BLK_FIRST; //block number? inode number?
+	DevReadBlock(block_num, pBlock);
+	memcpy(pBlock + (Inode_num % 8) * sizeof(__Inode), pInode, sizeof(__Inode));
+	DevWriteBlock(block_num, pBlock);	
+	free(pBlock);
 }
 
 
 void GetInode(int blkno, Inode* pInode)
 {
-	char* pblock = (char*)malloc(512);
-	int block_num = Inode_num / 8 + INODELIST_BLK_FIRST; //block number? inode number?
-	DevReadBlock(block_num, pblock);
-	__Inode pInode = (__Inode)malloc(sizeof(__Inode));
+	char* pBlock = (char*)malloc(BLOCK_SIZE);
+	int block_num = (Inode_num / 8) + INODELIST_BLK_FIRST; //block number? inode number?
+	DevReadBlock(block_num, pBlock);
+	memcpy(pInode, pBlock + (Inode_num % 8) * sizeof(__Inode), sizeof(__Inode));
+	free(pBlock);
 }
 
 
 int GetFreeInodeNum(void)
 {
-
+	char* pBlock = (char*)malloc(BLOCK_SIZE);
+	DevReadBlock(INODE_BITMAP_BLK_NUM, pBlock);
+	
+	for(int i = (BLOCK_SIZE * 8) - 1; i >= 0; i--){
+		if(!((*pBlock >> i) | 0)) return BLOCK_SIZE * 8 - i;	
+	}
 }
 
 
